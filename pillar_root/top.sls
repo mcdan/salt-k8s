72030cb3da4d0e64a0cf7aa357b205df3a925404
dan mcweeney <mcweeney@adobe.com>
2017-11-30 12:12:48 -0500

    Enable public-ip pillar generation

--- pillar_root/top.sls
+++ pillar_root/top.sls
@@ -1,3 +1,9 @@
 base:
   '*':
     - kubernetes
+    {%- for root in opts['pillar_roots'][saltenv] -%}
+      {%- if salt['file.file_exists']('{0}/public-ip.sls'.format(root)) %}
+    - public-ip
+      {% endif %}
+    {%- endfor %}
+    
--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-rsync -avz --delete --exclude '.git' --exclude 'file_root/certs' . coldplay.corp.adobe.com:~/cluster/salt-base
+rsync -avz --delete --exclude '.git' --exclude 'file_root/certs' --exclude 'pillar_root/public-ip.sls' . coldplay.corp.adobe.com:~/cluster/salt-base
 sleep 2
 done
 
