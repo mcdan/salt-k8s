287db3d1e101539722b26e5a33b6b880cc370902
dan mcweeney <mcweeney@adobe.com>
2017-12-13 13:43:45 -0500

    remove sync script

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true
-do
-rsync -avz --delete --exclude '.git' --exclude 'file_root/certs' --exclude 'pillar_root/public-ip.sls' --exclude 'pillar_root/num-workers.sls' . coldplay.corp.adobe.com:~/cluster/salt-base
-sleep 2
-done
-
